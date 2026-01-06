7e143825ed80bedda30e3f604b6f0aaaf208af1d
wei82800 <wei0828m@gmail.com>
2026-01-06 18:13:17 +0800

    Update 1_練習模式.py

--- exam_system/pages/1_練習模式.py
+++ exam_system/pages/1_練習模式.py
@@ -1,12 +1,3 @@
-import sys
-import os
-
-# 顯示當前工作目錄
-print("Current Working Directory:", os.getcwd())
-# 顯示 Python 搜尋路徑
-print("System Path:", sys.path)
-
-
 # exam_system/pages/1_練習模式.py
 import streamlit as st
 import time
